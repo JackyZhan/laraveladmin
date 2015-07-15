61bd8f5c00504d43062d46c4becafe218367aa23
JackyZhan <Jacky_zhan@foxmail.com>
2015-07-15 10:24:19 +0800

    update

--- app/event.php
+++ app/event.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class event extends Model {
+class Event extends Model {
 
-    //
     /**
      * The database table used by the model.
      *
      * @var string
      */
     protected $table = 'event';
+
 
 }
